51337161b91657f9d97080b1bcaf5ff8e673c0d8
a-3140 <aatienza.3140@gmail.com>
2022-05-29 00:27:04 +0800

    ui: Add search form

--- src/modules/weather/components/LoggedIn.vue
+++ src/modules/weather/components/LoggedIn.vue
@@ -1,15 +1,17 @@
 <script lang="ts" setup>
-const props = defineProps<{
+import SearchForm from "@/components/SearchForm.vue";
+
+defineProps<{
   userName: string;
   userGithub: string;
 }>();
 </script>
 
 <template>
-  <div>
+  <div class="max-w-md mx-auto">
     <p class="py-2">{{ userName }}</p>
     <p class="pt-2 pb-5">{{ userGithub }}</p>
-    <div class="py-5">Search should be here</div>
+    <SearchForm />
     <button
       type="button"
       class="px-3 py-1 hover:bg-gray-300 rounded-sm outline outline-2 outline-black"
